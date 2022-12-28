114ede4c863348b5a3c1a7e580e3bda2be13d806
Sushma Sharma <104353006+sushma1918@users.noreply.github.com>
2022-12-28 18:14:50 +0530

    Add files via upload
    A python program to implement Polymorphism.

---
+++ practical9.py
@@ -0,0 +1,33 @@
+'''
+Experiment 9
+Write a python program to implement Polymorphism.
+'''
+class India():
+	def capital(self):
+		print("New Delhi is the capital of India.")
+
+	def language(self):
+		print("Hindi is the most widely spoken language of India.")
+
+	def type(self):
+		print("India is a developing country.")
+
+class USA():
+	def capital(self):
+		print("Washington, D.C. is the capital of USA.")
+
+	def language(self):
+		print("English is the primary language of USA.")
+
+	def type(self):
+		print("USA is a developed country.")
+
+def funtion1(ob):
+	ob.capital()
+	ob.language()
+	ob.type()
+
+obj_ind = India()
+obj_usa = USA()
+funtion1(obj_ind)
+funtion1(obj_usa)
